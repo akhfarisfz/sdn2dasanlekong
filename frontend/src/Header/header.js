1e4d287fa28ed8dc43a4fa844e8235e8df392ee9
Lalu Ardita Arip <ardita.arip18@gmail.com>
2024-04-30 11:15:40 +0800

    lalu

--- frontend/src/Header/header.js
+++ frontend/src/Header/header.js
@@ -1,7 +1,14 @@
 import React from "react";
-
+import Tuturi from "../img/tuturi.png";
 function header() {
-  return <div>header</div>;
+  return (
+    <div className="flex bg-blue-400 ">
+      <div className="size-16 m-3 jus">
+        <img src={Tuturi} alt="image" />
+      </div>
+      <div></div>
+    </div>
+  );
 }
 
 export default header;
