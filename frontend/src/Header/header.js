35e2e0173d4ca697aaa5c187a8b4f69a4f8da728
Lalu Ardita Arip <ardita.arip18@gmail.com>
2024-04-30 18:15:09 +0800

    lalu
    header full

--- frontend/src/Header/header.js
+++ frontend/src/Header/header.js
@@ -1,14 +1,46 @@
-import React from "react";
-import Tuturi from "../img/tuturi.png";
-function header() {
+import React, { useState } from "react";
+import { CgMenuGridO } from "react-icons/cg";
+import { IoCloseCircleOutline } from "react-icons/io5";
+import Logo from "../img/tuturi.png";
+function Header() {
+  let Links = [
+    { name: "Sekolah", link: "/" },
+    { name: "Eksul", link: "/" },
+    { name: "Prestasi", link: "/" },
+  ];
+  let [isOpen, setIsOpen] = useState(false);
   return (
-    <div className="flex bg-blue-400 ">
-      <div className="size-16 m-3 jus">
-        <img src={Tuturi} alt="image" />
+    <div className=" shadow-md w-full bg-blue-400 fixed top-0 left-0 ">
+      <div className="md:px-10 py-4 px-7 md:flex justify-between items-center">
+        <div className="flex text-2xl cursor-pointer items-center gap-5">
+          <img className="size-12" src={Logo} alt="Logo" />
+          <span className="font-bold">SDN 2 Dasan Lekong</span>
+        </div>
+        <div
+          onClick={() => setIsOpen(!isOpen)}
+          className="w-7 h-7 absolute right-8 top-6 cursor-pointer md:hidden"
+        >
+          {isOpen ? (
+            <IoCloseCircleOutline className="size-8" />
+          ) : (
+            <CgMenuGridO className="size-8" />
+          )}
+        </div>
+
+        <ul
+          className={`md:flex md:pl-0 pl-9 md:items-center md:pb-0 pb-12 absolute md:static md:z-auto z-[-1] left-0 w-full md:w-auto  bg-blue-400  transition-all duration-500 ease-in ${
+            isOpen ? "top12" : "top-[-490px]"
+          }`}
+        >
+          {Links.map((link) => (
+            <li className="font-semibold my-7 md:my-0 md:ml-8">
+              <a href="/">{link.name}</a>
+            </li>
+          ))}
+        </ul>
       </div>
-      <div></div>
     </div>
   );
 }
 
-export default header;
+export default Header;
