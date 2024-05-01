872f87d6545e408e31ec871a7b3ee13238889b5d
muhilhamfebr26 <muhilhamfebr2@gmail.com>
2024-05-01 09:13:13 +0700

    nambahin footer

--- frontend/src/Home Page/Footer.js
+++ frontend/src/Home Page/Footer.js
@@ -1,1 +1,37 @@
 import React from "react";
+
+function Footer() {
+  return (
+    <footer className="bg-gray-900 text-white py-8 mt-10 w-screen">
+      <div className="container mx-6 px-4">
+        {/* Alamat Sekolah */}
+        <div className="mb-6">
+          <h3 className="text-xl font-semibold mb-2">Alamat Sekolah</h3>
+          <p>
+            Jl.Raden Patik Wadira - Dasan Lekong, Dasan Lekong, Kec. Sukamulia
+          </p>
+          <p>Lombok Timur, Nusa Tenggara Barat</p>
+        </div>
+        {/* Peta Lokasi */}
+        <div className="mb-6">
+          <h3 className="text-xl font-semibold mb-2">Lokasi Sekolah</h3>
+          <div className="aspect-w-16 aspect-h-9">
+            {/* Embedded Google Maps */}
+            <iframe
+              className="w-52 sm:w-1/3 md:w-1/4 lg:w-1/5 h-full"
+              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3944.559290796563!2d116.4918852!3d-8.6382364!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2dcc4bcfa28eb9e3%3A0xee89e3bde0587194!2sSDN%202%20DASAN%20LEKONG!5e0!3m2!1sid!2sid!4v1714476926486!5m2!1sid!2sid"
+              title="Google Maps"
+              allowFullScreen
+            ></iframe>
+          </div>
+        </div>
+        {/* Hak Cipta */}
+        <div className="text-center">
+          <p>&copy; 2024 Sekolah. Hak Cipta Dilindungi Undang-Undang</p>
+        </div>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
--- frontend/src/Home Page/Home.js
+++ frontend/src/Home Page/Home.js
@@ -1,8 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "../Header/Header.js";
-import sekolah from "../img/pexels-yankrukov-8617771.jpg"; // Impor gambar cover sekolah
+import Footer from "./Footer.js";
+import sekolah1 from "../img/Gambar-Gedung-Sekolah-Modern-18.jpg"; // Impor gambar cover sekolah
+
+const images = [
+  {
+    src: "image1.jpg",
+    title: "Judul Gambar 1",
+    description: "Deskripsi Gambar 1",
+  },
+  {
+    src: "image2.jpg",
+    title: "Judul Gambar 2",
+    description: "Deskripsi Gambar 2",
+  },
+  // Tambahkan item gambar dan caption sesuai kebutuhan
+];
 
 function Home() {
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  const prevSlide = () => {
+    setCurrentIndex((prevIndex) =>
+      prevIndex === 0 ? images.length - 1 : prevIndex - 1
+    );
+  };
+
+  const nextSlide = () => {
+    setCurrentIndex((prevIndex) =>
+      prevIndex === images.length - 1 ? 0 : prevIndex + 1
+    );
+  };
   return (
     <div className="container">
       <div
@@ -17,7 +45,7 @@ function Home() {
             Selamat Datang di halaman
           </h1>
           <p className="text-lg md:text-xl lg:text-2xl text-white mb-8">
-            SDN 02 Dasanlekong
+            SDN 2 Dasan Lekong
           </p>
           <div className="flex flex-col md:flex-row items-center justify-center md:justify-start">
             <button className="bg-white text-blue-500 hover:bg-blue-400 text-lg md:text-xl lg:text-2xl font-semibold py-2 px-6 md:py-3 md:px-8 lg:py-4 lg:px-10 rounded-full shadow-md transition duration-300 mb-4 md:mb-0">
@@ -27,10 +55,12 @@ function Home() {
         </div>
       </div>
       <div className="mt-16 px-4">
-        <h2 className="text-3xl font-semibold mb-4">Tentang Sekolah</h2>
+        <h2 className="text-3xl font-semibold mb-4">
+          Tentang SDN 2 Dasan Lekong
+        </h2>
         <div className="flex flex-col md:flex-row items-center md:items-start mb-8">
           <img
-            src={sekolah} // Gunakan prop src untuk menampilkan gambar cover sekolah
+            src={sekolah1} // Gunakan prop src untuk menampilkan gambar cover sekolah
             alt="Sekolah"
             className="w-full md:w-1/2 rounded-lg shadow-lg mb-4 md:mb-0"
           />
@@ -53,13 +83,38 @@ function Home() {
         <h2 className="text-3xl font-semibold mt-8 mb-4">
           Ekstrakurikuler dan Prestasi Sekolah
         </h2>
-        <p className="text-lg md:text-xl lg:text-2xl">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce ac
-          malesuada justo. Integer id orci quis felis dapibus scelerisque sed ut
-          nisl. Integer vulputate, nisi nec maximus suscipit, risus lacus tempus
-          ipsum, nec placerat arcu metus vel felis.
-        </p>
+        <div className="relative max-w-lg mx-auto">
+          {images.map((image, index) => (
+            <div
+              key={index}
+              className={index === currentIndex ? "block" : "hidden"}
+            >
+              <img
+                src={image.src}
+                alt={`Image ${index + 1}`}
+                className="w-full"
+              />
+              <div className="absolute bottom-0 left-0 w-full bg-black bg-opacity-50 text-white p-4">
+                <h3>{image.title}</h3>
+                <p>{image.description}</p>
+              </div>
+            </div>
+          ))}
+          <button
+            className="absolute top-1/2 transform -translate-y-1/2 left-0 rounded-full bg-black bg-opacity-50 text-white p-2"
+            onClick={prevSlide}
+          >
+            &#10094;
+          </button>
+          <button
+            className="absolute top-1/2 transform -translate-y-1/2 right-0 rounded-full bg-black bg-opacity-50 text-white p-2"
+            onClick={nextSlide}
+          >
+            &#10095;
+          </button>
+        </div>
       </div>
+      <Footer />
     </div>
   );
 }
