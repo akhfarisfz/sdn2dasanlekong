045c0a5da836e531926c873213eb05141dd8ad66
Lalu Ardita Arip <ardita.arip18@gmail.com>
2024-05-01 11:11:41 +0800

    lalu

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,15 @@
-import Header from "./Header/Header.js";
-import Home from "./Home Page/Home.js";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Login from "../src/Login Page/Login.js";
-
+import Home from "./Home Page/Home.js";
 
 function App() {
   return (
-    <div>
-      <Login/>
-      <Header />
-      <Home />
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/home" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
